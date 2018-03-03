79d7376e96a47879620a08ba67b58459c9165551
James Dindi <dindijjames@gmail.com>
2018-03-03 04:44:52 +0300

    Create user

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand 
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -15,9 +17,9 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
+    public_id = db.Column(db.String(200), unique=True)
     name = db.Column(db.String(50))
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(250))
     admin = db.Column(db.Boolean)
     todos = db.relationship('Todo', order_by='Todo.id', cascade='all, delete-orphan')
 
@@ -29,6 +31,33 @@ class Todo(db.Model):
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+@app.route('/user', methods=['GET'])
+def get_all_users():
+    return ""
+
+@app.route('/user/<user_id>', methods=['GET'])
+def get_one_user():
+    return ""
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created!'})
+
+@app.route('/user/<user_id>', methods=['PUT'])
+def promote_user():
+    return ""
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def delete_user():
+    return ""
+
 
 if __name__ == "__main__":
     app.run(debug=True)
