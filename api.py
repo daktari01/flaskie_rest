1e1038613138618445c699b489e0ba9c3714d564
James Dindi <dindijjames@gmail.com>
2018-03-03 10:36:22 +0300

    GET all users and GET one user by ID

--- api.py
+++ api.py
@@ -32,13 +32,33 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def get_all_users():
-    return ""
+    users = User.query.all()
+    output = []
+    # Get user data to a list of dictionaries
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-    return ""
+    return jsonify({'users' : output})
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message' : 'No user found'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    
+    return jsonify({'user' : user_data})
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -48,13 +68,13 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message': 'New user created!'})
+    return jsonify({'message' : 'New user created!'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
+@app.route('/user/<public_id>', methods=['PUT'])
 def promote_user():
     return ""
 
-@app.route('/user/<user_id>', methods=['DELETE'])
+@app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user():
     return ""
 
