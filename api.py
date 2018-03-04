1adbc933fbe1cb8eddfa4e2acc45e8d814d1f23a
James Dindi <dindijjames@gmail.com>
2018-03-04 09:45:39 +0300

    Add jwt authentication

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy 
 import uuid
+import jwt
 import psycopg2
+import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -12,7 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todo_db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(200), unique=True)
     name = db.Column(db.String(50))
@@ -22,15 +24,34 @@ class User(db.Model):
     
 
 class Todo(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message' : 'Token is missing'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
 
 @app.route('/users', methods=['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+
     users = User.query.all()
     output = []
     # Get user data to a list of dictionaries
@@ -45,7 +66,8 @@ def get_all_users():
     return jsonify({'users' : output})
 
 @app.route('/user/<public_id>', methods=['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message' : 'No user found'})
@@ -58,7 +80,8 @@ def get_one_user(public_id):
     return jsonify({'user' : user_data})
 
 @app.route('/user', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'])
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
@@ -68,7 +91,8 @@ def create_user():
     return jsonify({'message' : 'New user created!'})
 
 @app.route('/user/<public_id>', methods=['PUT'])
-def promote_user(public_id):
+@token_required
+def promote_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message' : 'No user found'})
@@ -77,7 +101,8 @@ def promote_user(public_id):
     return jsonify({'message' : 'The user has been promoted!'})
 
 @app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
     if not user:
         return jsonify({'message' : 'No user found'})
@@ -85,8 +110,25 @@ def delete_user(public_id):
     db.session.commit()
     return jsonify({'message' : 'The user has been deleted!'})
 
+@app.route('/login')
+def login():
+    auth = request.authorization
 
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
+    
+    user = User.query.filter_by(name=auth.username).first()
 
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 
+            'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 
+                app.config['SECRET_KEY'])    
+    
+        return jsonify({'token' : token.decode('UTF-8')})
+    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
 
 
 
